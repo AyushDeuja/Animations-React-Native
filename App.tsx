442b1378089f599f09bb1a029cbd02225b574e4a
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-22 14:42:27 +0545

    fadein and fade out

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import { useRef, useState } from 'react';
 import { Animated, Text, TouchableOpacity, View } from 'react-native';
 
 function App() {
-  const leftValue = useState(new Animated.Value(0))[0];
-  const moveBall = () => {
-    Animated.timing(leftValue, {
-      toValue: 1000,
+  const opacity = useState(new Animated.Value(0))[0];
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
       duration: 1000,
-      useNativeDriver: false,
+      useNativeDriver: true,
+    }).start();
+  };
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
     }).start();
   };
   return (
@@ -23,13 +30,16 @@ function App() {
         style={{
           width: 100,
           height: 100,
-          marginLeft: leftValue,
+          opacity,
           borderRadius: 100 / 2,
           backgroundColor: 'red',
         }}
       />
-      <TouchableOpacity onPress={moveBall}>
-        <Text>Press to move</Text>
+      <TouchableOpacity onPress={fadeIn}>
+        <Text>Press to fadeIn</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={fadeOut}>
+        <Text>Press to fadeOut</Text>
       </TouchableOpacity>
     </View>
   );
